32571183d9412b230f1d4b667b52bbb6b77f1558
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-25 19:37:59 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,38 @@
-<?php
+
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+
+<footer class="bg-light text-center text-lg-start">
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">A La Recherche Du Patio Perdu</h5>
+
+        <p>
+          A La Recherche Du Patio Perdu est un site internet réalisé par Diane-Devi Lacourte et Sara Juan 
+          pour vous permettre de retrouver ou de rencontrer des anciens de l'ENSC. Vous pourrez ainsi suivre 
+          leurs différents parcours mais aussi faire découvrir vos expériences ! 
+          Grâce à ce site, vous aurez l'impression d'être de retour à l'ENSC, à profiter du soleil et des 
+          ensciens sur notre formidable patio !  
+        </p>
+      
+    </div>
+  </div>
+
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    Et surtout vive l'
+    <a class="text-dark" href="https://www.ensc.fr">ENSC</a>
+  </div>
+</footer>
+
+<!-- note pour plus tard dans css : 
+.footer {
+    border-top: 1px solid #ccc;
+    padding-top: 10px;
+    margin-bottom: 10px;
+    text-align: center;
+}
+-->
