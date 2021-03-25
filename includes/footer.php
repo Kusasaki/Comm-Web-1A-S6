22db74f778333346ed259faa07329b8e2d49ff51
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-25 19:57:06 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 <footer class="bg-light text-center text-lg-start">
   <div class="container p-4">
     <div class="row">
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+      <div class="col-lg-12 col-md-12 mb-4 mb-md-0">
         <h5 class="text-uppercase">A La Recherche Du Patio Perdu</h5>
 
         <p>
