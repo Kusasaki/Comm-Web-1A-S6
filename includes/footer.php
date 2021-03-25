b13ffa28f1e5e345a7fb673a6d671d6aad424ad6
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-25 19:39:07 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-
+<html>
 <head>
     <meta charset='utf-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -36,3 +36,4 @@
     text-align: center;
 }
 -->
+</html>
