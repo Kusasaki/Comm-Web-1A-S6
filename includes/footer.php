be66432ce4df7937688094ef6d1a150a6834f50e
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-25 19:45:27 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,4 @@
-<html>
-<head>
-    <meta charset='utf-8'>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-</head>
+<?php include_once 'head.php'; ?>
 
 <footer class="bg-light text-center text-lg-start">
   <div class="container p-4">
