9647ecf4d956d9364c212c61a95384586a2b0763
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-27 14:46:08 +0100

    Update descpromo.php

--- descpromo.php
+++ descpromo.php
@@ -1,3 +1,6 @@
-<?php include_once "includes/head.php";?>
+<?php include_once "includes/head.php"; 
+    include_once "includes/navbar.php";?>
 
 <h2>La promotion <?php echo $_GET['promo']; ?></h2>
+
+<?php include_once "includes/footer.php"; ?>
