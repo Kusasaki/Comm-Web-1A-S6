28451f16bd9719c11065f8052efb3f9c00dfbe0d
Kusasaki <75423246+Kusasaki@users.noreply.github.com>
2021-03-27 19:54:49 +0100

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -3,19 +3,6 @@
 // Connect to the database. Returns a PDO object
 function getDb() {
     // Local deployment
-    /*$server = "localhost";
-    $username = "diane";
-    $password = "ensc";
-    $db = "kaonashi";*/
-    
-    // Deployment on Heroku with ClearDB for MySQL
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $server = $url["host"];
-    $username = $url["user"];<?php
-
-// Connect to the database. Returns a PDO object
-function getDb() {
-    // Local deployment
     $server = "localhost";
     $username = "diane";
     $password = "ensc";
@@ -33,27 +20,6 @@ function getDb() {
         }
         return $bdd;
     }*/
-}
-
-// Check if a user is connected
-function isUserConnected() {
-    return isset($_SESSION['login']);
-}
-
-// Redirect to a URL
-function redirect($url) {
-    header("Location: $url");
-}
-
-// Escape a value to prevent XSS attacks
-function escape($value) {
-    return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', false);
-}
-    $password = $url["pass"];
-    $db = substr($url["path"], 1);
-    
-    return new PDO("mysql:host=$server;dbname=$db;charset=utf8", "$username", "$password",
-    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 
 // Check if a user is connected
