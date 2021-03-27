0c17e940ebb65bc9d31f37aab164d993de434038
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-27 14:13:40 +0100

    Update promos.php

--- promos.php
+++ promos.php
@@ -1,1 +1,127 @@
-php
+<?php include_once "head.php";?>
+
+<h2 class="titre_page">Nos chères promotions :</h2>
+</br>
+
+<div class="card-columns">
+  <div class="card">
+  <img class="card-img-top" src="promo/promo2007_DeVinci.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2007</h5>
+      <p class="card-text">Voir les élèves de la promotion De Vinci</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2007">Voir la promo 2007</a>
+      </div>
+      </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="promo/promo2008_Varela.JPEG" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2008</h5>
+      <p class="card-text">Pomotion Varela</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2008">Voir la promo 2008</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="promo/promo2009_Turing.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2009</h5>
+      <p class="card-text">Pomotion Turing</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2009">Voir la promo 2007</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="promo/promo2010_Morin.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2010</h5>
+      <p class="card-text">Pomotion Morin</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2010">Voir la promo 2007</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="promo/promo2011_Truong.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2011</h5>
+      <p class="card-text">Promotion Truong</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2011">Voir la promo 2011</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="promo/promo2012_Norman.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2012</h5>
+      <p class="card-text">Promotion Norman</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2012">Voir la promo 2012</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="promo/promo2013_Asimov.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2013</h5>
+      <p class="card-text">Pomotion Asimov</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2013">Voir la promo 2013</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="promo/promo2014_Lerners-Lee.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2014</h5>
+      <p class="card-text">Pomotion Lerners-Lee</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2014">Voir la promo 2014</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="promo/promo2015_Lovelace.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2015</h5>
+      <p class="card-text">Pomotion Lovelace</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2015">Voir la promo 2015</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="promo/promo2016_Shannon.JPEG" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2016</h5>
+      <p class="card-text">Pomotion Shannon</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2016">Voir la promo 2016</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="promo/promo2017_Reason.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2017</h5>
+      <p class="card-text">Pomotion Reason</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2017">Voir la promo 2017</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="promo/promo2018_Hawking.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Promo 2018</h5>
+      <p class="card-text">Pomotion Hawking</p>
+      <div class="card-footer">
+        <a href ="descpromo.php?promo=2018">Voir la promo 2018</a>
+      </div>
+    </div>
+  </div>
+</div>
