7ef86743821390b2cbe36ccea0a99d9fad6cca70
Kusasaki <75423246+Kusasaki@users.noreply.github.com>
2021-03-27 19:53:58 +0100

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -11,7 +11,44 @@ function getDb() {
     // Deployment on Heroku with ClearDB for MySQL
     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $server = $url["host"];
-    $username = $url["user"];
+    $username = $url["user"];<?php
+
+// Connect to the database. Returns a PDO object
+function getDb() {
+    // Local deployment
+    $server = "localhost";
+    $username = "diane";
+    $password = "ensc";
+    $db = "kaonashi";
+    
+    /*try{
+        $bdd = new PDO(
+            "mysql:host=10.195.0.48;dbname=alarunru_anciens;charset=utf8",
+            "alarunru_admin",
+            "20kaonashi23",
+            array(PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION));
+        }
+        catch (Exception $e) {
+            die('Erreur fatale : ' . $e->getMessage());
+        }
+        return $bdd;
+    }*/
+}
+
+// Check if a user is connected
+function isUserConnected() {
+    return isset($_SESSION['login']);
+}
+
+// Redirect to a URL
+function redirect($url) {
+    header("Location: $url");
+}
+
+// Escape a value to prevent XSS attacks
+function escape($value) {
+    return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', false);
+}
     $password = $url["pass"];
     $db = substr($url["path"], 1);
     
