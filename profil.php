00238591c20895c32570a08cf01c99a8ca97d55a
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-27 16:31:30 +0100

    Update profil.php

--- profil.php
+++ profil.php
@@ -1,1 +1,14 @@
-php
+<?php include_once "includes/head.php";
+    include_once "includes/navbar.php"; ?>
+
+<h2 class = "titre_page">Profil</h2>
+</br>
+<div class="cadre"> 
+    <h6 class = "titre_rubrique">C'est qui ?</h6>
+
+    <h6 class = "titre_rubrique">Tu peux le contacter comment ?</h6>
+
+    <h6 class = "titre_rubrique">Il a fait quoi ?</h6>
+
+</div>
+<?php include_once "includes/footer.php"; ?>
