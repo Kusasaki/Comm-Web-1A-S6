fdb018139080752833bc61a0c621b894b38b8b5b
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-27 14:44:00 +0100

    Update promos.php

--- promos.php
+++ promos.php
@@ -1,11 +1,12 @@
-<?php include_once "head.php";?>
+<?php include_once "includes/head.php"; 
+    include_once 'includes/navbar.php'?>
 
 <h2 class="titre_page">Nos chères promotions :</h2>
 </br>
 
 <div class="card-columns">
   <div class="card">
-  <img class="card-img-top" src="promo/promo2007_DeVinci.jpg" alt="Card image cap">
+  <img class="card-img-top" src="images/promo/promo2007_DeVinci.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2007</h5>
       <p class="card-text">Voir les élèves de la promotion De Vinci</p>
@@ -15,7 +16,7 @@
       </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="promo/promo2008_Varela.JPEG" alt="Card image cap">
+    <img class="card-img-top" src="images/promo/promo2008_Varela.JPEG" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2008</h5>
       <p class="card-text">Pomotion Varela</p>
@@ -25,7 +26,7 @@
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="promo/promo2009_Turing.jpg" alt="Card image cap">
+    <img class="card-img-top" src="images/promo/promo2009_Turing.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2009</h5>
       <p class="card-text">Pomotion Turing</p>
@@ -35,7 +36,7 @@
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="promo/promo2010_Morin.jpg" alt="Card image cap">
+    <img class="card-img-top" src="images/promo/promo2010_Morin.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2010</h5>
       <p class="card-text">Pomotion Morin</p>
@@ -45,7 +46,7 @@
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="promo/promo2011_Truong.jpg" alt="Card image cap">
+    <img class="card-img-top" src="images/promo/promo2011_Truong.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2011</h5>
       <p class="card-text">Promotion Truong</p>
@@ -55,7 +56,7 @@
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="promo/promo2012_Norman.jpg" alt="Card image cap">
+    <img class="card-img-top" src="images/promo/promo2012_Norman.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2012</h5>
       <p class="card-text">Promotion Norman</p>
@@ -65,7 +66,7 @@
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="promo/promo2013_Asimov.jpg" alt="Card image cap">
+    <img class="card-img-top" src="images/promo/promo2013_Asimov.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2013</h5>
       <p class="card-text">Pomotion Asimov</p>
@@ -75,7 +76,7 @@
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="promo/promo2014_Lerners-Lee.jpg" alt="Card image cap">
+    <img class="card-img-top" src="images/promo/promo2014_Lerners-Lee.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2014</h5>
       <p class="card-text">Pomotion Lerners-Lee</p>
@@ -85,7 +86,7 @@
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="promo/promo2015_Lovelace.jpg" alt="Card image cap">
+    <img class="card-img-top" src="images/promo/promo2015_Lovelace.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2015</h5>
       <p class="card-text">Pomotion Lovelace</p>
@@ -95,7 +96,7 @@
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="promo/promo2016_Shannon.JPEG" alt="Card image cap">
+    <img class="card-img-top" src="images/promo/promo2016_Shannon.JPEG" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2016</h5>
       <p class="card-text">Pomotion Shannon</p>
@@ -105,7 +106,7 @@
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="promo/promo2017_Reason.jpg" alt="Card image cap">
+    <img class="card-img-top" src="images/promo/promo2017_Reason.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2017</h5>
       <p class="card-text">Pomotion Reason</p>
@@ -115,7 +116,7 @@
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="promo/promo2018_Hawking.jpg" alt="Card image cap">
+    <img class="card-img-top" src="images/promo/promo2018_Hawking.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Promo 2018</h5>
       <p class="card-text">Pomotion Hawking</p>
@@ -125,3 +126,6 @@
     </div>
   </div>
 </div>
+<?php
+include_once 'includes/footer.php';
+?>
