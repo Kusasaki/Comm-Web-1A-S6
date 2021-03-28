ae810b59eaa57e0454e85b9ab37e99682ccc95cb
Kusasaki <75423246+Kusasaki@users.noreply.github.com>
2021-03-28 14:12:49 +0200

    Update mesexp.php

--- mesexp.php
+++ mesexp.php
@@ -1,5 +1,25 @@
-<?php include_once "includes/head.php";
-    include_once "navbar.php"; ?>
+<?php include_once "includes/head.php"; 
+    include_once "includes/header.php";
+    $eleve = $_GET['eleve'];
+    $exp = get_exp_by_student($eleve);
+	$res = $exp->fetchAll(); ?>
+<html>
+    
+    <h2 class="titre_page">Les expériences de <?php echo "$eleve"; ?></h2>
+    </br>
+    <p class="titre_page"> Les expériences :</p>
+    <div class="cadre">
+        <?php 
+        
+        foreach( $res as $ligne) 
+            { $id_organisation= $ligne['id_organisation'] ; 
+            $date_debut = $ligne['date_debut']?>
+            $date_fin = $ligne['date_fin']?>
+            <a class="center" href="descexp.php?nom_eleve=<?php echo "$nom"?>&prenom=<?php echo "$prenom"?>"><?php echo "$nom $prenom" ?></a><br/>
+            <?php
+            } 
+?>
+    </div>
 
-<h2 class="titre_page">Mes expériences !</h2>
-</br>
+</html>
+<?php include_once "includes/footer.php"; ?>
