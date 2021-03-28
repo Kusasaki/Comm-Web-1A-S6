668adf754c9810d7aec4e9eb824bcb107d1a2b8c
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-28 16:46:35 +0200

    Update mesexp.php

--- mesexp.php
+++ mesexp.php
@@ -9,8 +9,10 @@
             { $nom = $ligne['nom_eleve'];
                 $prenom = $ligne['prenom_eleve'];
             }
+
     $eleve = get_xp($nom, $prenom);
-	$res = $eleve->fetchAll();?>
+    $res = $eleve->fetchAll();
+    ?>
 
 <html>
     <?php include_once "includes/header.php";?>
@@ -19,26 +21,45 @@
     
         <?php 
         
-            
+        ?><div class="cadre"><?php
             foreach( $res as $ligne) 
-            { ?> <div class="cadre"> <?php
-                if ($ligne['etat'] == 0)
-                {
-                    echo "Type d'expérience :". $ligne['type_exp']; 
+            { ?>  <p class="titre_page">Expérience : </p> <?php
+                
+                    echo "Type d'expérience : ". $ligne['type_exp']; 
                     ?> <br/> <?php
-                    echo "Brève description de l'expérience :". $ligne['description_exp']; 
+                    echo "Brève description de l'expérience : ". $ligne['description_exp']; 
                     ?> <br/> <?php
-                    echo "Durée de l'expérience :". $ligne['date_debut']." - ".$ligne['date_fin']; 
+                    echo "Durée de l'expérience : ". $ligne['date_debut']." - ".$ligne['date_fin']; 
                     ?> <br/> <?php
-                    echo "Salaire :". $ligne['salaire']; 
+                    echo "Salaire : ". $ligne['salaire']; 
                     ?> <br/> <?php
+                    if ($ligne['etat'] == 0)echo "Etat : visible";
+                    else echo "Etat : invisible";
+
+                    $id_organisation=$ligne['id_organisation'];
+                    $organisation = get_organisation($id_organisation);
+                    $resu = $organisation->fetchAll();
+                    foreach( $resu as $ligne) 
+                        { ?> <p class="titre_page">Organisation : </p> <?php
+                            echo "Nom : ". $ligne['nom_organisation']; 
+                            ?> <br/> <?php
+                            echo "Type d'organisation : ". $ligne['type_organisation']; 
+                            ?> <br/> <?php 
+                            echo "Secteur d'activité : ". $ligne['secteur_activite']; 
+                            ?> <br/> <?php
+                            echo "Adresse : ". $ligne['ad_postale']." ".$ligne['code_postal_organisation']." ".$ligne['ville_organisation']; 
+                            ?> <br/> <?php
+                            echo "Téléphone : ". $ligne['telephone_organisation']; 
+                            ?> <br/> <?php
+                        }
                 }
-               ?> </div> <?php
-            }
+               ?> </div><br/>
 
-	?>
+            <form  name="x" action="addexp.php" method="post"><input class="ajouter" type="submit" value="Ajouter une expérience"></form>
+            
+            
 
-        <form name="x" action="addexp.php" method="post"><input type="submit" value="Ajouter une expérience"></form>
+            <?php include_once "includes/footer.php";
+        ?>
 
-        <?php include_once "includes/footer.php"; ?>
-
+</html>
