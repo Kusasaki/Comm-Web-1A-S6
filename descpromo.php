76b1c5fdcc02e4fe161ecbc2d8631adcea2d8221
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-28 10:49:22 +0200

    Update descpromo.php

--- descpromo.php
+++ descpromo.php
@@ -1,6 +1,24 @@
 <?php include_once "includes/head.php"; 
-    include_once "includes/navbar.php";?>
+    include_once "includes/header.php";
+    $promo = $_GET['promo'];
+    $eleve = get_student_by_promo($promo);
+	$res = $eleve->fetchAll(); ?>
+<html>
+    
+    <h2 class="titre_page">La promotion <?php echo "$promo"; ?></h2>
+    </br>
+    <p class="titre_page"> Les élèves de la promo :</p>
+    <div class="cadre">
+        <?php 
+        
+        foreach( $res as $ligne) 
+            { $nom = $ligne['nom_eleve'] ; 
+            $prenom = $ligne['prenom_eleve']?>
+            <a class="center" href="desceleve.php?nom_eleve=<?php echo "$nom"?>"><?php echo "$nom $prenom" ?></a><br/>
+            <?php
+            } 
+?>
+    </div>
 
-<h2>La promotion <?php echo $_GET['promo']; ?></h2>
-
+</html>
 <?php include_once "includes/footer.php"; ?>
