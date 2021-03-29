a927d0c6f32ffeb2e8ad9f6d68b6e09c1ab353c2
Kusasaki <75423246+Kusasaki@users.noreply.github.com>
2021-03-29 20:25:32 +0200

    Update mesexp.php

--- mesexp.php
+++ mesexp.php
@@ -16,50 +16,44 @@
 
 <html>
     <?php include_once "includes/header.php";?>
-    <h2 class="titre_page">Mes expériences !</h2>
-    </br>
-    
-        <?php 
+    <body>
+        <h2 class="titre_page">Mes expériences !</h2>
+        </br>
+            <div>
+                <?php
+                    ?><div><?php 
 
-            foreach( $res as $ligne) 
-            { ?> <div class="cadre"> <p class="titre_page">Expérience : </p> <?php
+                    foreach( $res as $ligne) 
+                    { ?> <div class="cadre"> <p class="titre_page">Expérience : </p> <?php
+                        
+                        echo "Type d'expérience : ". $ligne['type_exp']; 
+                        ?> <br/> <?php
+                        echo "Brève description de l'expérience : ". $ligne['description_exp']; 
+                        ?> <br/> <?php
+                        echo "Durée de l'expérience : ". $ligne['date_debut']." - ".$ligne['date_fin']; 
+                        ?> <br/> <?php
+                        echo "Salaire : ". $ligne['salaire']; 
+                        ?> <br/> <?php
+                        if ($ligne['etat'] == 0)echo "Etat : visible";
+                        else echo "Etat : invisible";
+                        
+                        ?>
+                        </div><br/>
+
+                        <form  name="modifierexp" action="modifierexp.php" method="post"><button type="submit">Modifier l'expérience</button><input type="hidden" name="id" value="<?= $ligne['id_exppro'] ?>" ></input></form><br/>
+
+                        <form  name="supexp" action="supexp.php?id= <?php echo $ligne['id_exppro']?>" method="post"><button type="submit">Supprimer l'expérience</button></form>
+                        <br/>
+
+                    <?php } ?>
+            </div>
+            <div>
+                <hr/><br/>
+
+                <form  name="ajoutexp" action="addexp.php" method="post"><button type="submit">Ajouter une expérience</button></form>
+                <br/>
                 
-                    echo "Type d'expérience : ". $ligne['type_exp']; 
-                    ?> <br/> <?php
-                    echo "Brève description de l'expérience : ". $ligne['description_exp']; 
-                    ?> <br/> <?php
-                    echo "Durée de l'expérience : ". $ligne['date_debut']." - ".$ligne['date_fin']; 
-                    ?> <br/> <?php
-                    echo "Salaire : ". $ligne['salaire']; 
-                    ?> <br/> <?php
-                    if ($ligne['etat'] == 0)echo "Etat : visible";
-                    else echo "Etat : invisible";
-
-                    $id_organisation=$ligne['id_organisation'];
-                    $organisation = get_organisation($id_organisation);
-                    $resu = $organisation->fetchAll();
-                    foreach( $resu as $ligne) 
-                        { ?> <p class="titre_page">Organisation : </p> <?php
-                            echo "Nom : ". $ligne['nom_organisation']; 
-                            ?> <br/> <?php
-                            echo "Type d'organisation : ". $ligne['type_organisation']; 
-                            ?> <br/> <?php 
-                            echo "Secteur d'activité : ". $ligne['secteur_activite']; 
-                            ?> <br/> <?php
-                            echo "Adresse : ". $ligne['ad_postale']." ".$ligne['code_postal_organisation']." ".$ligne['ville_organisation']; 
-                            ?> <br/> <?php
-                            echo "Téléphone : ". $ligne['telephone_organisation']; 
-                            ?> <br/> <?php
-                        }
-                    ?></div><br/><?php
-                }
-               ?> <br/>
-
-            <form  name="x" action="addexp.php" method="post"><input class="ajouter" type="submit" value="Ajouter une expérience"></form>
-            
-            
-
-            <?php include_once "includes/footer.php";
-        ?>
-
+            </div>
+            <?php include_once "includes/footer.php";?>
+        </body>
 </html>
