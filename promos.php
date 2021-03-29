46688b05b9a9f4bfa34825b7286f0044679a7720
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-29 21:30:34 +0200

    Update promos.php

--- promos.php
+++ promos.php
@@ -12,120 +12,120 @@
   <div class="card">
   <img class="card-img-top" src="images/promo/promo2007_DeVinci.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2007</h5>
+      <h5 class="card-title">Promotion 2007</h5>
       <p class="card-text">Voir les élèves de la promotion De Vinci</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2007">Voir la promo 2007</a>
+        <a href ="descpromo.php?promo=2007">Voir la promotion 2007</a>
       </div>
       </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/promo/promo2008_Varela.JPEG" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2008</h5>
-      <p class="card-text">Pomotion Varela</p>
+      <h5 class="card-title">Promotion 2008</h5>
+      <p class="card-text">Promotion Varela</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2008">Voir la promo 2008</a>
+        <a href ="descpromo.php?promo=2008">Voir la promotion 2008</a>
       </div>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/promo/promo2009_Turing.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2009</h5>
-      <p class="card-text">Pomotion Turing</p>
+      <h5 class="card-title">Promotion 2009</h5>
+      <p class="card-text">Promotion Turing</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2009">Voir la promo 2007</a>
+        <a href ="descpromo.php?promo=2009">Voir la promotion 2009</a>
       </div>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/promo/promo2010_Morin.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2010</h5>
+      <h5 class="card-title">Promotion 2010</h5>
       <p class="card-text">Pomotion Morin</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2010">Voir la promo 2007</a>
+        <a href ="descpromo.php?promo=2010">Voir la promotion 2010</a>
       </div>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/promo/promo2011_Truong.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2011</h5>
+      <h5 class="card-title">Promotion 2011</h5>
       <p class="card-text">Promotion Truong</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2011">Voir la promo 2011</a>
+        <a href ="descpromo.php?promo=2011">Voir la promotion 2011</a>
       </div>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/promo/promo2012_Norman.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2012</h5>
+      <h5 class="card-title">Promotion 2012</h5>
       <p class="card-text">Promotion Norman</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2012">Voir la promo 2012</a>
+        <a href ="descpromo.php?promo=2012">Voir la promotion 2012</a>
       </div>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/promo/promo2013_Asimov.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2013</h5>
-      <p class="card-text">Pomotion Asimov</p>
+      <h5 class="card-title">Promotion 2013</h5>
+      <p class="card-text">Promotion Asimov</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2013">Voir la promo 2013</a>
+        <a href ="descpromo.php?promo=2013">Voir la promotion 2013</a>
       </div>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/promo/promo2014_Lerners-Lee.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2014</h5>
-      <p class="card-text">Pomotion Lerners-Lee</p>
+      <h5 class="card-title">Promotion 2014</h5>
+      <p class="card-text">Promotion Lerners-Lee</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2014">Voir la promo 2014</a>
+        <a href ="descpromo.php?promo=2014">Voir la promotion 2014</a>
       </div>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/promo/promo2015_Lovelace.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2015</h5>
-      <p class="card-text">Pomotion Lovelace</p>
+      <h5 class="card-title">Promotion 2015</h5>
+      <p class="card-text">Promotion Lovelace</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2015">Voir la promo 2015</a>
+        <a href ="descpromo.php?promo=2015">Voir la promotion 2015</a>
       </div>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/promo/promo2016_Shannon.JPEG" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2016</h5>
-      <p class="card-text">Pomotion Shannon</p>
+      <h5 class="card-title">Promotion 2016</h5>
+      <p class="card-text">Promotion Shannon</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2016">Voir la promo 2016</a>
+        <a href ="descpromo.php?promo=2016">Voir la promotion 2016</a>
       </div>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/promo/promo2017_Reason.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2017</h5>
-      <p class="card-text">Pomotion Reason</p>
+      <h5 class="card-title">Promotion 2017</h5>
+      <p class="card-text">Promotion Reason</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2017">Voir la promo 2017</a>
+        <a href ="descpromo.php?promo=2017">Voir la promotion 2017</a>
       </div>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/promo/promo2018_Hawking.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Promo 2018</h5>
-      <p class="card-text">Pomotion Hawking</p>
+      <h5 class="card-title">Promotion 2018</h5>
+      <p class="card-text">Promotion Hawking</p>
       <div class="card-footer">
-        <a href ="descpromo.php?promo=2018">Voir la promo 2018</a>
+        <a href ="descpromo.php?promo=2018">Voir la promotion 2018</a>
       </div>
     </div>
   </div>
