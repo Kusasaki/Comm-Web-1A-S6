d54e42c79793550d19b1a6b54d04adb82bc135c6
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-03-29 16:56:19 +0200

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -50,56 +50,45 @@ function escape($value) {
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', false);
 }
 
-// fonction permettantd de chercher dans la bd tous les élèves d'une promo
+// fonction permettant de chercher dans la bd tous les élèves d'une promo
 function get_student_by_promo($promo){
-    /**
-     * Return specified columns for all movies
-     *
-     * @param Array   $columns  columns to retrieve
-     *
-     * @throws PDOException If specified columns are not in table movie
-     * @return Array $all_movies with all movies
-     */
     $request = getDb() -> prepare("SELECT * FROM eleve WHERE annee=?");
     $request->execute(array($promo));
     return $request;
 }
-
+//fonction requete permettant d'accéder à toutes les informations d'un élève
 function get_info_from_eleve($nom, $prenom){
-    /**
-     * Return specified columns for all movies
-     *
-     * @param Array   $columns  columns to retrieve
-     *
-     * @throws PDOException If specified columns are not in table movie
-     * @return Array $all_movies with all movies
-     */
     $request = getDb() -> prepare("SELECT * FROM eleve WHERE nom_eleve=? AND prenom_eleve=?");
     $request->execute(array($nom,$prenom));
     return $request;
 }
+//fonction requete permettant d'obtenir l'identifiant d'un état grâce au nom et au prénom d'un élève
 function get_id_etat($nom, $prenom){
     $request=getDB() -> prepare("SELECT id_etat FROM eleve WHERE nom_eleve=? AND prenom_eleve=?");
     $request->execute(array($nom, $prenom));
     return $request;
 }
+//fonction requete qui permet d'accéder aux états des données d'un élève
 function is_available($id_etat){
     $request = getDB() -> prepare("SELECT * FROM etat WHERE id_etat=?");
     $request -> execute(array($id_etat));
     return $request;
 }
+//fonction requete permettant d'accéder à une expérience
 function get_xp($nom, $prenom){
     $request=getDB() -> prepare("SELECT * FROM experiencepro, eleve WHERE nom_eleve=? AND prenom_eleve=?
     AND eleve.id_eleve = experiencepro.id_eleve");
     $request -> execute(array($nom, $prenom));
     return $request;
 }
+//fonction requete permettant d'obtenir le nom et le prénom d'un utilisateur
 function get_utilisateur($user){
     $request=getDB() -> prepare("SELECT nom_eleve, prenom_eleve FROM eleve, acces WHERE acces.id_eleve = eleve.id_eleve
     AND acces.nom_utilisateur=?");
     $request -> execute(array($user));
     return $request;
 }
+//fonction requete permettant d'accéder à une information
 function get_organisation($id_organisation){
     $request=getDB() -> prepare("SELECT * FROM organisation WHERE id_organisation=?");
     $request -> execute(array($id_organisation));
