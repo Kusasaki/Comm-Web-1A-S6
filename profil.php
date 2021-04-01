685e09e7def6619f4a3226721dac9efebc4a6452
Kusasaki <75423246+Kusasaki@users.noreply.github.com>
2021-04-01 23:57:34 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -101,7 +101,7 @@ include_once "includes/functions.php";
         {
             foreach( $res as $ligne) 
             { 
-                echo "Nom :". $ligne['nom_eleve']; 
+                echo "Nom :" . $ligne['nom_eleve']; 
                 ?> <br/> <?php
                 echo "Prenom : " .$ligne['prenom_eleve'];
                 ?> <br/> <?php
@@ -117,7 +117,7 @@ include_once "includes/functions.php";
                   </svg> <?php
                   }
                 ?> <br/> <?php
-                echo "Date de naissance : " .$ligne['date_naissance'];
+                echo "Date de naissance : " .$ligne['date_naissance']; ?><br/><?php
                 echo "Téléphone : " .$ligne['telephone_eleve'];
                 if ($li['telephone_eleve']==0){?>   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-unlock-fill" viewBox="0 0 16 16">
                     <path d="M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2z"/>
