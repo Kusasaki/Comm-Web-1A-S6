8adb27ea8602a1093ebf2cdd56a79d218523e6f9
PabloGri <74774797+PabloGri@users.noreply.github.com>
2021-04-02 11:09:32 +0200

    Update supexp.php

--- supexp.php
+++ supexp.php
@@ -1,10 +1,11 @@
 <?php
 	include_once "includes/functions.php";
 	$id = $_POST['id'];
+	print_r($_POST);	
 
     $supexp  = getDb()->prepare('delete from experiencepro where id_exppro = ?');
     $supexp->execute(array($id));
-    redirect("mesexp.php");
+    //redirect("mesexp.php");
 ?>
 
 
