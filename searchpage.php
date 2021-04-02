bf274ce49d48adb9784425d0192e4b9318e3c4f8
SaraJuann <75420764+SaraJuann@users.noreply.github.com>
2021-04-02 11:34:37 +0200

    Update searchpage.php

--- searchpage.php
+++ searchpage.php
@@ -1,143 +1,128 @@
 <?php session_start();
     include_once "includes/head.php";
-    include_once "includes/functions.php";
     include_once "includes/header.php"; 
-    ?>
-    <h2 class="titre_page">Recherche</h2>
-    </br>
+    $table = $_GET['table'];
+    $attribut = $_GET['attribut'];
+    $valeur = $_GET['valeur'];
     
-    <?php
-    if(isUserConnected()){?>
-        <form method="POST" action="searchpage.php" role="form">
-        <fieldset class="cadre">
-        <br/>
-        <div class="btn-group ">
-            <button type="button" class="btn btn-light dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Recherche d'une promotion
-            </button>
-            <div class="dropdown-menu dropdown-menu-right ">
-<!-- faire une sorte de foreach ligne in column ... affiche titre et return qqch-->
-            <?php 
-                $table="promotion";
-                $attribut="annee";
-                $promo = get_list_attribut($table, $attribut);
-                $res = $promo->fetchAll();
-                foreach ($res as $ligne){
-                    $info = $ligne['annee'];
-                    ?>
-                    <a class="dropdown-item" type="button" href="resultresearch.php?table=<?php echo "$table"?>&attribut=<?php echo "$attribut"?>&valeur=<?php echo "$info"?>"><?php echo "$info"?></a>
-                    <?php
-                } ?>
-            </div>
-        </div>
-
-        <div class="btn-group">
-            <button type="button" class="btn btn-light dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Recherche d'un ancien par son nom
-            </button>
-
-            <div class="dropdown-menu dropdown-menu-right">
-            <?php 
-                $table="eleve";
-                $attribut="nom_eleve";
-                $promo = get_list_attribut($table, $attribut);
-                $res1 = $promo->fetchAll();
-                foreach ($res1 as $ligne){
-                    $info = $ligne['nom_eleve'];
-                    ?>
-                        <a class="dropdown-item" type="button" href="resultresearch.php?table=<?php echo "$table"?>&attribut=<?php echo "$attribut"?>&valeur=<?php echo "$info"?>"><?php echo "$info"?></a>
-                    <?php
-                } ?>
-            </div>
-        </div>
-        <br/><br/>
-        
-        <div class="btn-group">
-            <button type="button" class="btn btn-light dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Type d'expérience
-            </button>
-
-            <div class="dropdown-menu dropdown-menu-right">
-            <?php 
-                $table="experiencepro";
-                $attribut="type_exp";
-                $promo = get_list_attribut($table, $attribut);
-                $res = $promo->fetchAll();
-                foreach ($res as $ligne){
-                    $info = $ligne['type_exp'];
-                    ?>
-                    <a class="dropdown-item" type="button" href="resultresearch.php?table=<?php echo "$table"?>&attribut=<?php echo "$attribut"?>&valeur=<?php echo "$info"?>"><?php echo "$info"?></a>
-                    <?php
-                } ?>
-            </div>
-        </div>
-
-        <div class="btn-group">
-            <button type="button" class="btn btn-light dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Secteur d'activité
-            </button>
-
-            <div class="dropdown-menu dropdown-menu-right">
-            <?php 
-                $table="organisation";
-                $attribut="secteur_activite";
-                $promo = get_list_attribut($table, $attribut);
-                $res = $promo->fetchAll();
-                foreach ($res as $ligne){
-                    $info = $ligne['secteur_activite'];
-                    ?>
-                    <a class="dropdown-item" type="button" href="resultresearch.php?table=<?php echo "$table"?>&attribut=<?php echo "$attribut"?>&valeur=<?php echo "$info"?>"><?php echo "$info"?></a>
-                    <?php
-                } ?>
-            </div>
-        </div>
-        <br/><br/>
-        <div class="btn-group">
-            <button type="button" class="btn btn-light dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Recherche organisation par nom
-            </button>
-            <div class="dropdown-menu dropdown-menu-right">
-            <?php 
-                $table="organisation";
-                $attribut="nom_organisation";
-                $promo = get_list_attribut($table, $attribut);
-                $res = $promo->fetchAll();
-                foreach ($res as $ligne){
-                    $info = $ligne['nom_organisation'];
-                    ?>
-                    <a class="dropdown-item" type="button" href="resultresearch.php?table=<?php echo "$table"?>&attribut=<?php echo "$attribut"?>&valeur=<?php echo "$info"?>"><?php echo "$info"?></a>
-                    <?php
-                } ?>
-            </div>
-        </div>
-        <div class="btn-group">
-            <button type="button" class="btn btn-light dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Recherche d'une organisation par lieu
-            </button>
-            <div class="dropdown-menu dropdown-menu-right">
-            <?php 
-                $table="organisation";
-                $attribut="ville_organisation";
-                $promo = get_list_attribut($table, $attribut);
-                $res = $promo->fetchAll();
-                foreach ($res as $ligne){
-                    $info = $ligne['ville_organisation'];
-                    ?>
-                    <a class="dropdown-item" type="button" href="resultresearch.php?table=<?php echo "$table"?>&attribut=<?php echo "$attribut"?>&valeur=<?php echo "$info"?>"><?php echo "$info"?></a>
-                    <?php
-                } ?>
-            </div>
-        <br/><br/>
-        </div>
-</fieldset>
-</form>
-    <?php
+    //recherche d'une promo
+    if($table == "promotion"){
+        $red = "descpromo.php?promo=".$valeur;
+        redirect($red);
     }
-    else {
+
+//recherche d'un élève
+    if($table == "eleve"){
+        $request = getDb() -> prepare("SELECT prenom_eleve FROM eleve WHERE nom_eleve=?");
+        $request->execute(array($valeur));
+        $res = $request->fetch();
+        $prenom = $res['prenom_eleve'];
+        $red="desceleve.php?nom=".$valeur."&prenom=".$prenom;
+        redirect($red);
+    }
+
+//recherche type d'expérience
+    if($table=="experiencepro"){
         ?>
-        <h4 class="titre_rubrique">Désolé, il semble que vous n'êtes pas connecté.e,<br/>Vous ne pouvez donc pas accéder à la page de recherche</h4>
-        <br/>
-        <div class="titre_rubrique">Veuillez vous <a href="login.php">connecter</a> ou vous <a href="inscription.php">inscrire</a> !</div>
+        <h4 class="titre_rubrique">Résultat de la recherche "<?php echo "$valeur"?>"</h4>
         <?php
+        $request = getDb() -> prepare("SELECT * FROM experiencepro WHERE type_exp=? AND etat=0;");
+        $request->execute(array($valeur));
+        $res=$request->fetchAll();
+        foreach($res as $ligne){
+            ?>
+            <div class="cadre">
+            <?php
+            echo "Description de l'expérience : ". $ligne['description_exp']; 
+            ?> <br/> <?php
+            echo "Durée de l'expérience : ". $ligne['date_debut']." - ".$ligne['date_fin']; 
+            ?> <br/> <?php
+            echo "Salaire : ". $ligne['salaire']; 
+            ?> <br/></div><br/><?php
+        }
+        if($request->rowCount() == 0) {echo "Personne n'a souhaité partager son expérience";}
     }
+
+//recherche par propriétés liées à la table organisation
+    if($table == "organisation"){
+// recherche par secteur d'activité
+        if($attribut == "secteur_activite"){
+        ?>
+        <h4 class="titre_rubrique">Résultat de la recherche "<?php echo "$valeur"?>"</h4>
+        <?php
+        $request=getDB()->prepare("SELECT * FROM organisation WHERE secteur_activite=?");
+        $request->execute(array($valeur));
+        $res = $request->fetchAll();
+        } 
+        if($attribut == "nom_organisation"){
+            ?>
+            <h4 class="titre_rubrique">Résultat de la recherche "<?php echo "$valeur"?>"</h4>
+            <?php
+            $request = getDB()->prepare("SELECT * FROM organisation WHERE nom_organisation=?");
+            $request -> execute(array($valeur));
+            $res = $request->fetchAll();
+        }
+        if($attribut == "ville_organisation"){
+            ?>
+            <h4 class="titre_rubrique">Résultat de la recherche "<?php echo "$valeur"?>"</h4>
+            <?php
+            $request=getDB()->prepare("SELECT * FROM organisation WHERE ville_organisation=?");
+            $request->execute(array($valeur));
+            $res = $request->fetchAll();
+        }
+        if (isset($res)){
+            foreach($res as $ligne) 
+            { ?>
+                <div class="cadre">
+                <?php
+                echo "Nom : ". $ligne['nom_organisation']; 
+                ?> <br/> <?php
+                echo "Type d'organisation : ". $ligne['type_organisation']; 
+                ?> <br/> <?php 
+                echo "Secteur d'activité : ". $ligne['secteur_activite']; 
+                ?> <br/> <?php
+                echo "Adresse : ". $ligne['ad_postale']." ".$ligne['code_postal_organisation']." ".$ligne['ville_organisation']; 
+                ?> <br/> <?php
+                echo "Téléphone : ". $ligne['telephone_organisation']; 
+                ?> <br/></div><br/> <?php
+
+                $requestExp=getDB()->prepare("SELECT * FROM experiencepro WHERE id_organisation =? AND etat = 0");
+                $requestExp->execute(array($ligne['id_organisation']));
+                $res = $requestExp->fetchAll();
+                ?>
+                <h5 class = "titre_rubrique">Expériences des anciens de l'ENSC</h5>
+                </br>
+                <!-- Afficher les expériences liées à cette organisation-->
+                <?php
+                foreach($res as $ligne){
+                    ?>
+                    <div class="cadre">
+                    <?php
+                    echo "Description de l'expérience : ". $ligne['description_exp']; 
+                    ?> <br/> <?php
+                    echo "Durée de l'expérience : ". $ligne['date_debut']." - ".$ligne['date_fin']; 
+                    ?> <br/> <?php
+                    echo "Salaire : ". $ligne['salaire']; 
+                    ?> <br/><br/><?php
+                    //Cherche la personne ayant réalisé cette expérience et propose d'aller voir son profil
+                    $request = getDb() -> prepare("SELECT nom_eleve, prenom_eleve FROM eleve WHERE id_eleve=?");
+                    $request->execute(array($ligne['id_eleve']));
+                    $resEleve = $request->fetch();
+                    $nom = $resEleve['nom_eleve'];
+                    $prenom = $resEleve['prenom_eleve'];
+                    $red="desceleve.php?nom=".$nom."&prenom=".$prenom;
+                    ?>
+                    Personne ayant réalisé cette expérience : <?php echo "$prenom $nom"?>
+                    <br/>
+                    <a href="<?php echo"$red"?>">Voir le profil de <?php echo "$prenom $nom"?></a>
+                    <?php
+                    ?></div><br/><?php
+                }
+                if($requestExp->rowCount() == 0) {echo "Personne n'a souhaité partager son expérience";}
+
+            }
+        }
+    }
+    
     ?>
+    
